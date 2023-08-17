23e27a592acff122310a5fcdcb33aa0ea57edd16
ErezTheVeryMuchCool <142494378+ErezTheVeryMuchCool@users.noreply.github.com>
2023-08-17 13:11:55 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function bclick()
 {
-  window.open("https://youtu.be/gh36do2Zkfg"[, "_blank");
+  window.open("https://youtu.be/gh36do2Zkfg"[,"_blank");
   setTimeout(1000)
   alert("Got rickrolled!");
 }
